97971a7fbf129fa13074370830d88a8fbbdfe955
Gichiri Gichuru <gichirigichuru@gmail.com>
2022-10-21 13:34:25 +0300

    add event handlers

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 let num1 = 8
-let num2 = 6
+let num2 = 2
 
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+
+let sumEl =document.getElementById("sum-el")
+
 function add(){
-    console.log(num1+num2)
+    let result = num1+num2
+    sumEl.textContent ="Sum :"+result
 }
 add()
+
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum :"+result
+}
+subtract()
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum :" + result
+}
+divide()
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum :" + result
+}
+multiply()
